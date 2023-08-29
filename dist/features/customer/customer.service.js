907ebdd5d525bea5e249ba3b5d43f4f1308b1928
Hugo Souza <hugo.souza@mbmsolutions.com.br>
2023-08-28 23:57:33 -0300

    Migrando

--- dist/features/customer/customer.service.js
+++ dist/features/customer/customer.service.js
@@ -35,7 +35,6 @@ let CustomerService = exports.CustomerService = class CustomerService {
                 },
             };
         });
-        console.log('customer.services.findAll -> Result', res);
         return res;
     }
     async create(dto) {
