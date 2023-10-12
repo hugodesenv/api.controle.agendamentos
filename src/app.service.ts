e827cfb52e6a1d0acc33c8594c6a847abd6327b3
Hugo Souza <hugo.souza@mbmsolutions.com.br>
2023-10-12 00:18:55 -0300

    Melhorias

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Ol√°, a API est√° online!';
+    return 'Ol·, API est· Online!';
   }
 }
